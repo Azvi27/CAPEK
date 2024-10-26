3a454c1ee1f01b1d09e9471434781302f9471772
Azvi27 <120187050+Azvi27@users.noreply.github.com>
2024-10-26 20:53:04 +0700

    Update ModbusRtu.h

--- ModbusRtu.h
+++ ModbusRtu.h
@@ -1,58 +1,9 @@
-/**
- * @file 	ModbusRtu.h
- * @version     1.21
- * @date        2016.02.21
- * @author 	Samuel Marco i Armengol
- * @contact     sammarcoarmengol@gmail.com
- * @contribution Helium6072
- * @contribution gabrielsan
- *
- * @description
- *  Arduino library for communicating with Modbus devices
- *  over RS232/USB/485 via RTU protocol.
- *
- *  Further information:
- *  http://modbus.org/
- *  http://modbus.org/docs/Modbus_over_serial_line_V1_02.pdf
- *
- * @license
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; version
- *  2.1 of the License.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * @defgroup setup Modbus Object Instantiation/Initialization
- * @defgroup loop Modbus Object Management
- * @defgroup buffer Modbus Buffer Management
- * @defgroup discrete Modbus Function Codes for Discrete Coils/Inputs
- * @defgroup register Modbus Function Codes for Holding/Input Registers
- *
- */
-
 #ifndef MODBUS_RTU_H
 #define MODBUS_RTU_H
 
 #include <inttypes.h>
 #include "Arduino.h"
 
-
-/**
- * @struct modbus_t
- * @brief
- * Master query structure:
- * This includes all the necessary fields to make the Master generate a Modbus query.
- * A Master may keep several of these structures and send them cyclically or
- * use them according to program needs.
- */
 typedef struct
 {
     uint8_t u8id;          /*!< Slave address between 1 and 247. 0 means broadcast */
@@ -70,11 +21,6 @@ enum
     CHECKSUM_SIZE = 2
 };
 
-/**
- * @enum MESSAGE
- * @brief
- * Indexes to telegram frame positions
- */
 enum MESSAGE
 {
     ID                             = 0, //!< ID field
@@ -86,15 +32,6 @@ enum MESSAGE
     BYTE_CNT  //!< byte counter
 };
 
-/**
- * @enum MB_FC
- * @brief
- * Modbus function codes summary.
- * These are the implement function codes either for Master or for Slave.
- *
- * @see also fctsupported
- * @see also modbus_t
- */
 enum MB_FC
 {
     MB_FC_NONE                     = 0,   /*!< null operator */
@@ -148,12 +85,6 @@ const unsigned char fctsupported[] =
 #define T35  5
 #define  MAX_BUFFER  64	//!< maximum size for the communication buffer in bytes
 
-/**
- * @class Modbus
- * @brief
- * Arduino class library for communicating with Modbus devices over
- * USB/RS232/485 (via RTU protocol).
- */
 class Modbus
 {
 private:
@@ -206,21 +137,14 @@ public:
     void setTxendPinOverTime( uint32_t u32overTime );
     void end(); //!<finish any communication and release serial communication port
 
-    //
-    // Deprecated functions
-
-    // Deprecated: Use constructor: "Modbus m(0,Serial,0)" instead.
     Modbus(uint8_t u8id=0, uint8_t u8serno=0, uint8_t u8txenpin=0) __attribute__((deprecated));
 
-    // Deprecated: Use "start()" instead.
     template<typename T_Stream>
     void begin(T_Stream* port_, long u32speed_) __attribute__((deprecated));
 
-    // Deprecated: Use "start()" instead.
     template<typename T_Stream>
     void begin(T_Stream* port_, long u32speed_, uint8_t u8txenpin_) __attribute__((deprecated));
 
-    // Deprecated: Use "start()" instead.
     void begin(long u32speed = 19200) __attribute__((deprecated));
 };
 
